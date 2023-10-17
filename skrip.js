5fe839229d8433ce10a25fcd2309ad3bcc20466d
FahrulNa <140799150+FahrulNa@users.noreply.github.com>
2023-10-17 11:32:10 +0700

    skrip.js

--- skrip.js
+++ skrip.js
@@ -1,30 +1,13 @@
-function show_list() {
-  var courses = document.getElementById("courses_id");
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-  if (courses.style.display == "block") {
-      courses.style.display = "none";
-  } else {
-      courses.style.display = "block";
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+   if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
   }
 }
-window.onclick = function (event) {
-  if (!event.target.matches('.dropdown_button')) {
-      document.getElementById('courses_id')
-          .style.display = "none";
-  }
-}  
-
-const navbarToggle = navbar.querySelector("#navbar-toggle");
-const navbarMenu = document.querySelector("#navbar-menu");
-const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
-let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
-
-const toggleNavbarVisibility = () => {
-  isNavbarExpanded = !isNavbarExpanded;
-  navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
-};
-
-navbarToggle.addEventListener("click", toggleNavbarVisibility);
-
-navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
-navbarMenu.addEventListener("click", toggleNavbarVisibility)
